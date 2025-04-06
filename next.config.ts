a53ab614ee0e0f875a82c44b06353d416c7347e5
sh5 <hi.shu5.go93@gmail.com>
2025-04-06 23:36:14 +0900

    Fixed a bug

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: `export`,
   images: {
-    domains: ["source.unsplash.com", "pbs.twimg.com", "waujtfrtdrmkrnnsnvtb.supabase.co"],
+    domains: ["source.unsplash.com", "pbs.twimg.com", "waujtfrtdrmkrnnsnvtb.supabase.co", "muse-board.pages.dev"],
   },
 };
 
