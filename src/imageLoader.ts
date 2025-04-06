b00dc725f72e17d98acfb1709e29b1a401f9a213
sh5 <hi.shu5.go93@gmail.com>
2025-04-07 00:07:34 +0900

    Fixed a bug

--- src/imageLoader.ts
+++ src/imageLoader.ts
@@ -1,16 +1,12 @@
-export default function cloudflareLoader({
-  src,
-  width,
-  quality,
-}: {
-  src: string;
-  width: number;
-  quality?: number;
-}) {
+import { ImageLoader } from 'next/image';
+
+const cloudflareLoader: ImageLoader = ({ src, width, quality }) => {
   const params = [`width=${width}`];
   if (quality) {
     params.push(`quality=${quality}`);
   }
   const paramsString = params.join(',');
-  return `https://muse-board.pages.dev/cdn-cgi/image/${paramsString}/${src}`;
-}
+  return `https://muse-board.pages.dev/cdn-cgi/image/${paramsString}${src}`;
+};
+
+export default cloudflareLoader;
